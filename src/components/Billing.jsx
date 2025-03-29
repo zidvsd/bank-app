45072d2b5199d2207b9ed4ad4cfacd1b5cc41dd7
zidvsd <rashidvisda@gmail.com>
2025-03-29 19:48:58 +0800

    .

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 const Billing = () => {
-  return <div></div>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] relative h-[100%] z-[5]"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
