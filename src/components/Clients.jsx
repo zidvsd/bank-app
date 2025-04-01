6c605b038c2f1349ed3412f31491460c4797589c
Zid Visda <rashidvisda@gmail.com>
2025-04-01 15:09:14 +0800

    Added Testimonial Section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import styles from "../style";
+import { clients } from "../constants";
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      {/* <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter}  sm:min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              className="object-contain sm:w-[192px] w-[100px] "
+              alt=""
+            />
+          </div>
+        ))}
+      </div> */}
+    </section>
+  );
 };
-
 export default Clients;
--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>;
+import { quotes } from "../assets";
+const FeedbackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-md max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img
+        src={quotes}
+        alt="quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p className="font-poppins leading-[32px] text-secondary my-10 font-normal">
+        {content}Z
+      </p>
+      <div className="flex flex-row">
+        <img src={img} alt="name" className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4 ">
+          <h4 className="font-poppins leading-[32px] text-xl text-secondary font-semibold ">
+            {name}
+          </h4>
+          <p className="font-poppins leading-[24px] text-dimWhite font-normal">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackCard;
